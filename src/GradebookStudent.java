0714440499a40b5d2a6ab85b5e76e0b6d6f47e1d
Austin Kuriakose <austinkuriakose@Austins-MacBook-Air.local>
2026-07-14 15:38:30 -0400

    edited gradebook student

--- src/GradebookStudent.java
+++ src/GradebookStudent.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+
 public class GradebookStudent {
-    
+    private int id;
+    private String name;
+    private ArrayList<GradeItem> grades;
+
+    public GradebookStudent() {
+        grades = new ArrayList<>();
+    }
+
+    public GradebookStudent(int i, String n, ArrayList<GradeItem> g) {
+        if(id > 0) i = id;
+        
+        if(!name.trim().equals(null) || !name.trim().equals("")) name = n;
+        
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
+
