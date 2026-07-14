a4558901665a6bd075d6b0e99feace076c22381d
austinkuriakose95-cloud <austinkuriakose95@gmail.com>
2026-07-14 16:19:21 -0400

    acc finished gradebookstudent

--- src/GradebookStudent.java
+++ src/GradebookStudent.java
@@ -10,13 +10,28 @@ public class GradebookStudent {
     }
 
     public GradebookStudent(int i, String n) {
-        if(id > 0) i = id;
+        if(id < 0) throw new IllegalArgumentException("Enter a valid ID.");
         
-        if(!name.trim().equals(null) || !name.trim().equals("")) name = n;
+        if(name.trim().equals(null) || name.trim().equals("")) throw new IllegalArgumentException("Write a proper name.");
         
+        i = id;
+        n = name;
+    }
+
+    public String describe() {
+        return name + "has a " + calculateAverage() + " average.";
     }
 
 
+    public double calculateAverage() {
+        if(grades.size()==0) return 0.0;
+        else {int sum = 0;
+        for (GradeItem g : grades) {
+            sum += g.getScore();
+        }
+        return sum/grades.size();
+        }
+    }
     public int getId() {
         return id;
     }
