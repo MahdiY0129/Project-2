beb7397b6fdd156363dc0827b733d98da69e1c4e
MahdiY0129 <yousuf.mahdi.hasan@gmail.com>
2026-07-14 14:10:04 -0400

    changed consturcturo

--- src/GradeItem.java
+++ src/GradeItem.java
@@ -9,13 +9,9 @@ public class GradeItem {
         if(t.trim() == null || t.trim().length() == 0) 
             throw new IllegalArgumentException("Invalid input, title can't be empty or null!");
 
-
-        try{
             score = s; 
             title = t; 
-        }catch(IllegalArgumentException e){
-            System.out.println(e.getMessage());
-        }
+        
     }    
 
     public String getTitle(){return title;}
@@ -26,11 +22,9 @@ public class GradeItem {
         if(s<0.0 && s>100.0) 
             throw new IllegalArgumentException("Invalid input, grade must be above 0!");
 
-        try{
-            score = s; 
-        }catch(IllegalArgumentException e){
-            System.out.println(e.getMessage());
-        }
+        
+        score = s; 
+    
     }
 
 
