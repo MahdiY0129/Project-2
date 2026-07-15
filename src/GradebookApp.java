9a5dcb1a9878764f3ecfcc71fbfabf8c0a85889e
MahdiY0129 <yousuf.mahdi.hasan@gmail.com>
2026-07-15 16:34:04 -0400

    updated gradebookmanager and main menu

--- src/GradebookApp.java
+++ src/GradebookApp.java
@@ -1,5 +1,67 @@
+import java.util.Scanner; 
+
 public class GradebookApp {
     public static void main(String[] args) {
+        boolean exit = false;
+        System.out.println("==== Gradebook Manager ===="); 
+        Scanner sc = new Scanner(System.in); 
         
+        while(exit == false){
+            try{
+                printMenu(); 
+                int choice = sc.nextInt(); 
+
+                switch (choice) {
+                    case 1:
+                        
+                        break;
+
+                    case 2:
+
+                        break;
+                        
+                    case 3:
+                        
+                        break;
+
+                    case 4:
+
+                        break;
+                    
+                    case 5:
+                        
+                        break;
+
+                    case 6:
+
+                        break;
+
+                    case 7:
+                        
+                        break;
+
+                    case 8:
+                        exit = true; 
+                        break;
+                
+                    default:
+                        throw new IllegalArgumentException("Invalid input, number is not in menu!"); 
+                }
+            } catch(IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void printMenu(){
+        System.out.println("1. Add Student");
+        System.out.println("2. Add Grade to Student");
+        System.out.println("3. View All Students");
+        System.out.println("4. View Student Details");
+        System.out.println("5. Search Student by ID");
+        System.out.println("6. Load Data from File");
+        System.out.println("7. Save Data to File");
+        System.out.println("8. Exit");
+        System.out.println("Enter choice: ");
     }
 }
--- src/GradebookManager.java
+++ src/GradebookManager.java
@@ -1,3 +1,49 @@
+import java.util.ArrayList;
+
 public class GradebookManager {
+    private ArrayList<GradebookStudent> students; 
+
+    public GradebookManager(){
+        students = new ArrayList<>(); 
+    }
+
+    public void addStudent(GradebookStudent e){
+
+        if(isValidID(e) == true){
+            students.add(e); 
+        }
+        else throw new IllegalArgumentException("Invalid input, ID already exists"); 
+
+    }
+
+    public GradebookStudent findByID(int id){
+        for (GradebookStudent s : students) {
+            if (s.getId() == id) return s; 
+        }
+
+        return null; 
+    }
+
+    public boolean isValidID(GradebookStudent e){
+        for (GradebookStudent s : students) {
+            if (e.getId() == s.getId()) return false; 
+        }
+
+        return true; 
+    }
+
+    public void viewStudents(){
+        for (GradebookStudent s : students) {
+            System.out.println("Student name: " + s.getName());
+        }
+    }
+
+    public void addGradeToStudent(int id, String title, double score){
+        for (GradebookStudent s : students) {
+            if (s.getId() == id){
+
+            }
+        }
+    }
     
 }
